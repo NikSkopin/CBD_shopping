e53da35c71d977f31c83002ba0a21c3680411138
Nikita <skopin.n@gmail.com>
2019-05-21 17:58:25 +0200

    products section and JS for tabs

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,20 @@
-(
+
     // navigation
 
     // tabs
+function opentab(evt, tabNumber) {
+    const tabcontent = document.getElementsByClassName('tabcontent');
+    const tablink = document.getElementById(tabNumber);
     
+    for (let i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].classList.remove('showtab');
+    }
 
-)
+    const tablinks = document.getElementsByClassName('tablinks');
+        for (let i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(' active', '');
+        }
+
+    tablink.className +=' showtab';
+    evt.currentTarget.className += ' active';
+}
